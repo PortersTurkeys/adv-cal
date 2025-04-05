2755e107f8cc8ed66bbc507a23a66dfe614ebada
PortersTurkeys <portersrareturkeys@gmail.com>
2025-04-05 09:28:00 -0400

    Update PredefinedCrosses.js

--- PredefinedCrosses.js
+++ PredefinedCrosses.js
@@ -1,39 +1,36 @@
-const scriptUrls = [
-"https://porterturkeys.com/PDC/SirePhenos.js", 
-"https://porterturkeys.com/PDC/DamPhenos.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses1.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses2.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses3.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses4.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses5.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses6.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses7.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses8.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses9.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses10.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses11.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses12.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses13.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses14.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses15.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses16.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses17.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses18.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses19.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses20.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses21.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses22.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses23.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses24.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses25.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses26.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses27.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses28.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses29.js",
-"https://porterturkeys.com/PDC/PredefinedCrosses30.js"
-
-
-    
+const scriptUrls = [ 
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/SirePhenos.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/DamPhenos.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses1.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses2.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses3.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses4.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses5.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses6.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses7.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses8.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses9.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses10.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses11.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses12.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses13.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses14.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses15.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses16.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses17.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses18.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses19.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses20.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses21.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses22.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses23.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses24.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses25.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses26.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses27.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses28.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses29.js",
+    "https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses30.js"
 ];
 
 scriptUrls.forEach(url => {
@@ -43,124 +40,124 @@ scriptUrls.forEach(url => {
     document.head.appendChild(script);
 });
 
-
-
+// Manually append scripts 1â€“30
 const script1 = document.createElement('script');
-script1.src = 'https://porterturkeys.com/PDC/PredefinedCrosses1.js';
+script1.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses1.js';
 document.head.appendChild(script1);
 
 const script2 = document.createElement('script');
-script2.src = 'https://porterturkeys.com/PDC/PredefinedCrosses2.js';
+script2.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses2.js';
 document.head.appendChild(script2);
 
 const script3 = document.createElement('script');
-script3.src = 'https://porterturkeys.com/PDC/PredefinedCrosses3.js';
+script3.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses3.js';
 document.head.appendChild(script3);
 
 const script4 = document.createElement('script');
-script4.src = 'https://porterturkeys.com/PDC/PredefinedCrosses4.js';
+script4.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses4.js';
 document.head.appendChild(script4);
 
 const script5 = document.createElement('script');
-script5.src = 'https://porterturkeys.com/PDC/PredefinedCrosses5.js';
+script5.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses5.js';
 document.head.appendChild(script5);
 
 const script6 = document.createElement('script');
-script6.src = 'https://porterturkeys.com/PDC/PredefinedCrosses6.js';
+script6.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses6.js';
 document.head.appendChild(script6);
 
 const script7 = document.createElement('script');
-script7.src = 'https://porterturkeys.com/PDC/PredefinedCrosses7.js';
+script7.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses7.js';
 document.head.appendChild(script7);
 
 const script8 = document.createElement('script');
-script8.src = 'https://porterturkeys.com/PDC/PredefinedCrosses8.js';
+script8.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses8.js';
 document.head.appendChild(script8);
 
 const script9 = document.createElement('script');
-script9.src = 'https://porterturkeys.com/PDC/PredefinedCrosses9.js';
+script9.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses9.js';
 document.head.appendChild(script9);
 
 const script10 = document.createElement('script');
-script10.src = 'https://porterturkeys.com/PDC/PredefinedCrosses10.js';
+script10.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses10.js';
 document.head.appendChild(script10);
 
 const script11 = document.createElement('script');
-script11.src = 'https://porterturkeys.com/PDC/PredefinedCrosses11.js';
+script11.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses11.js';
 document.head.appendChild(script11);
 
 const script12 = document.createElement('script');
-script12.src = 'https://porterturkeys.com/PDC/PredefinedCrosses12.js';
+script12.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses12.js';
 document.head.appendChild(script12);
 
 const script13 = document.createElement('script');
-script13.src = 'https://porterturkeys.com/PDC/predefinedCrosses13.js';
+script13.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses13.js';
 document.head.appendChild(script13);
 
 const script14 = document.createElement('script');
-script14.src = 'https://porterturkeys.com/PDC/predefinedCrosses14.js';
+script14.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses14.js';
 document.head.appendChild(script14);
 
 const script15 = document.createElement('script');
-script15.src = 'https://porterturkeys.com/PDC/predefinedCrosses15.js';
+script15.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses15.js';
 document.head.appendChild(script15);
 
 const script16 = document.createElement('script');
-script16.src = 'https://porterturkeys.com/PDC/predefinedCrosses16.js';
+script16.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses16.js';
 document.head.appendChild(script16);
 
 const script17 = document.createElement('script');
-script17.src = 'https://porterturkeys.com/PDC/predefinedCrosses17.js';
+script17.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses17.js';
 document.head.appendChild(script17);
 
 const script18 = document.createElement('script');
-script18.src = 'https://porterturkeys.com/PDC/predefinedCrosses18.js';
+script18.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses18.js';
 document.head.appendChild(script18);
 
 const script19 = document.createElement('script');
-script19.src = 'https://porterturkeys.com/PDC/predefinedCrosses19.js';
+script19.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses19.js';
 document.head.appendChild(script19);
 
 const script20 = document.createElement('script');
-script20.src = 'https://porterturkeys.com/PDC/predefinedCrosses20.js';
+script20.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses20.js';
 document.head.appendChild(script20);
 
 const script21 = document.createElement('script');
-script21.src = 'https://porterturkeys.com/PDC/predefinedCrosses21.js';
+script21.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses21.js';
 document.head.appendChild(script21);
 
 const script22 = document.createElement('script');
-script22.src = 'https://porterturkeys.com/PDC/predefinedCrosses22.js';
+script22.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses22.js';
 document.head.appendChild(script22);
 
 const script23 = document.createElement('script');
-script23.src = 'https://porterturkeys.com/PDC/predefinedCrosses23.js';
+script23.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses23.js';
 document.head.appendChild(script23);
 
 const script24 = document.createElement('script');
-script24.src = 'https://porterturkeys.com/PDC/predefinedCrosses24.js';
+script24.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses24.js';
 document.head.appendChild(script24);
 
 const script25 = document.createElement('script');
-script25.src = 'https://porterturkeys.com/PDC/predefinedCrosses25.js';
+script25.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses25.js';
 document.head.appendChild(script25);
 
 const script26 = document.createElement('script');
-script26.src = 'https://porterturkeys.com/PDC/predefinedCrosses26.js';
+script26.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses26.js';
 document.head.appendChild(script26);
 
 const script27 = document.createElement('script');
-script27.src = 'https://porterturkeys.com/PDC/predefinedCrosses27.js';
+script27.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses27.js';
 document.head.appendChild(script27);
 
 const script28 = document.createElement('script');
-script28.src = 'https://porterturkeys.com/PDC/predefinedCrosses28.js';
+script28.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses28.js';
 document.head.appendChild(script28);
 
 const script29 = document.createElement('script');
-script29.src = 'https://porterturkeys.com/PDC/predefinedCrosses29.js';
+script29.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses29.js';
 document.head.appendChild(script29);
 
 const script30 = document.createElement('script');
-script30.src = 'https://porterturkeys.com/PDC/predefinedCrosses30.js';
+script30.src = 'https://porters-rare-heritage-turkeys.neocities.org/PDC/PredefinedCrosses30.js';
 document.head.appendChild(script30);
+
