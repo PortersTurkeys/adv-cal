3f769a163b5dff4d3d071eb45deb01dee76a7d21
PortersTurkeys <portersrareturkeys@gmail.com>
2025-04-13 12:15:37 -0400

    Create PredefinedCrosses8.js

---
+++ BeginnerVersion/PDC/PredefinedCrosses8.js
@@ -0,0 +1,48 @@
+const predefinedCrosses8 = [
+// bb WHITE CROSSES
+{ sire: 'White (Dark Brown Eyes)', dam: 'Bronze', results: { male: ['Bronze'], maleGenotype: ['bb Cc'], maleRatio: [1], female: ['Bronze'], femaleGenotype: ['bb Cc'], femaleRatio: [1] }},
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Black', results: { male: ['Barred Black'], maleGenotype: ['Bb Cc'], maleRatio: [1], female: ['Barred Black'], femaleGenotype: ['Bb Cc'], femaleRatio: [1] }},
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Bourbon Red', results: { male: ['Red Bronze'], maleGenotype: ['bb Cc Rr'], maleRatio: [1], female: ['Red Bronze'], femaleGenotype: ['bb Cc Rr'], femaleRatio: [1] }},
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Blue Red Bronze', results: { male: ['Red Slate', 'Blue Red Bronze', 'Bronze', 'Red Bronze'], maleGenotype: ['bb Cc Dd', 'bb Cc Dd Rr', 'bb Cc', 'bb Cc Rr'], maleRatio: [1, 1, 1, 1], female: ['Red Slate', 'Blue Red Bronze', 'Bronze', 'Red Bronze'], femaleGenotype: ['bb Cc Dd', 'bb Cc Dd Rr', 'bb Cc', 'bb Cc Rr'], femaleRatio: [1, 1, 1, 1] }},
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Chocolate', results: { male: ['Barred Black'], maleGenotype: ['Bb Cc Ee'], maleRatio: [1], female: ['Barred Black'], femaleGenotype: ['Bb Cc'], femaleRatio: [1] }},
+
+{sire: 'White (Dark Brown Eyes)', dam: 'Fall Fire', results: {male: ['Bronze-', 'Red Bronze.'], maleGenotype: ['bb1 cgc', 'bb1 cgc Rr'], maleRatio: [25, 25], female: ['Bronze-', 'Red Bronze.'], femaleGenotype: ['bb1 cgc', 'bb1 cgc Rr'], femaleRatio: [25, 25]}},
+
+{sire: 'White (Dark Brown Eyes)', dam: 'White (Blue Eyes)', results: {male: ['White (Blue Eyes)'], maleGenotype: ['Bb cc'], maleRatio: [50], female: ['White (Blue Eyes)'], femaleGenotype: ['Bb cc'], femaleRatio: [50]}},
+
+
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Oregon Gray', results: { male: ['Bronze', 'White (Dark Brown Eyes)'], maleGenotype: ['bb cgc Nn', 'bb cc Nn'], maleRatio: [25, 25], female: ['Bronze', 'White (Dark Brown Eyes)'], femaleGenotype: ['bb cgc', 'bb cc'], femaleRatio: [25, 25] } },
+
+{ sire: 'White (Dark Brown Eyes)', dam: 'Oregon Brown', results: { male: ['Bronze', 'White (Dark Brown Eyes)'], maleGenotype: ['bb cgc Ee Nn', 'bb cc Ee Nn'], maleRatio: [25, 25], female: ['Bronze', 'White (Dark Brown Eyes)'], femaleGenotype: ['bb cgc', 'bb cc'], femaleRatio: [25, 25] } },
+
+
+
+
+
+// BB White Crosses
+{ sire: 'White (Blue Eyes)', dam: 'Chocolate', results: { male: ['Black'], maleGenotype: ['BB Cc Ee'], maleRatio: [50], female: ['Black'], femaleGenotype: ['BB Cc'], femaleRatio: [50] } },
+
+{ sire: 'White (Blue Eyes)', dam: 'Mottled Black', results: { male: ['Black', 'Tri-color Mottled Black'], maleGenotype: ['BB cgc Nn', 'Bb1 cgc Nn'], maleRatio: [25, 25], female: ['Black', 'Tri-color Mottled Black'], femaleGenotype: ['BB cgc', 'Bb1 cgc'], femaleRatio: [25, 25] } },
+
+{ sire: 'White (Blue Eyes)', dam: 'Narragansett', results: { male: ['Barred Black'], maleGenotype: ['Bb Cc Nn'], maleRatio: [50], female: ['Barred Black'], femaleGenotype: ['Bb Cc'], femaleRatio: [50] } },
+
+{ sire: 'White (Blue Eyes)', dam: 'Slate', results: { male: ['Slate', 'Black'], maleGenotype: ['BB Cc Dd', 'BB Cc'], maleRatio: [25, 25], female: ['Slate', 'Black'], femaleGenotype: ['BB Cc Dd', 'BB Cc'], femaleRatio: [25, 25] } },
+
+  
+
+
+
+];
+
+
+
+
+
+
+
+
