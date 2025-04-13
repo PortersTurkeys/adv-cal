f39926f1b0243882e4468893e0c123fcf15dfc20
PortersTurkeys <portersrareturkeys@gmail.com>
2025-04-13 12:16:52 -0400

    Create PredefinedCrosses9.js

---
+++ BeginnerVersion/PDC/PredefinedCrosses9.js
@@ -0,0 +1,20 @@
+const predefinedCrosses9 = [
+  // Bb Chocolate Painted 
+ { sire: 'Chocolate Painted (Split Bronze)', dam: 'Chocolate Painted (Split Bronze)', results: { male: ['Chocolate Tuxedo', 'Grizzled Chocolate Painted', 'White (Blue Eyes)', 'Barred Chocolate Tuxedo', 'Chocolate Painted (Split Bronze)', 'White (Blue Eyes)', 'Mottled Auburn', 'Chocolate Fawn', 'White (Dark Brown Eyes)'], maleGenotype: ['BB cmcm ee', 'BB cmc ee', 'BB cc ee', 'Bb cmcm ee', 'Bb cmc ee', 'Bb cc ee', 'bb cmcm ee', 'bb cmc ee', 'bb cc ee'], maleRatio: [3.125, 6.25, 3.125, 6.25, 12.5, 6.25, 3.125, 6.25, 3.125], female: ['Chocolate Tuxedo', 'Grizzled Chocolate Painted', 'White (Blue Eyes)', 'Barred Chocolate Tuxedo', 'Chocolate Painted (Split Bronze)', 'White (Blue Eyes)', 'Mottled Auburn', 'Chocolate Fawn', 'White (Dark Brown Eyes)'], femaleGenotype: ['BB cmcm e-', 'BB cmc e-', 'BB cc e-', 'Bb cmcm e-', 'Bb cmc e-', 'Bb cc e-', 'bb cmcm e-', 'bb cmc e-', 'bb cc e-'], femaleRatio: [3.125, 6.25, 3.125, 6.25, 12.5, 6.25, 3.125, 6.25, 3.125] } },
+
+{ sire: 'Chocolate Painted (Split Bronze)', dam: 'Auburn', results: { male: ['Barred Chocolate', 'Barred Chocolate', 'Auburn', 'Auburn'], maleGenotype: ['Bb Ccm ee', 'Bb Cc ee', 'bb Ccm ee', 'bb Cc ee'], maleRatio: [12.5, 12.5, 12.5, 12.5], female: ['Barred Chocolate', 'Barred Chocolate', 'Auburn', 'Auburn'], femaleGenotype: ['Bb Ccm e-', 'Bb Cc e-', 'bb Ccm e-', 'bb Cc e-'], femaleRatio: [12.5, 12.5, 12.5, 12.5] } },
+ 
+  
+  
+  
+  
+  
+  ];
+
+
+
+
+
+
+
+
