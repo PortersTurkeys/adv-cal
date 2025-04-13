092120fa5a2643de0fa63bd91364d3b4051ed22a
PortersTurkeys <portersrareturkeys@gmail.com>
2025-04-13 12:07:45 -0400

    Create PredefinedCrosses5.js

---
+++ BeginnerVersion/PDC/PredefinedCrosses5.js
@@ -0,0 +1,28 @@
+const predefinedCrosses6 = [
+  
+// NEBRASKAN CROSSES
+{ sire: 'Nebraskan', dam: 'Nebraskan', results: { male: ['Nebraskan'], maleGenotype: ['bb spsp'], maleRatio: [50], female: ['Nebraskan'], femaleGenotype: ['bb spsp'], femaleRatio: [50] } },
+
+{ sire: 'Nebraskan', dam: 'Bronze', results: { male: ['Bronze'], maleGenotype: ['bb Spsp'], maleRatio: [50], female: ['Bronze'], femaleGenotype: ['bb Spsp'], femaleRatio: [50] } },
+
+{ sire: 'Nebraskan', dam: 'Bourbon Red', results: { male: ['Red Bronze'], maleGenotype: ['bb Rr Spsp'], maleRatio: [50], female: ['Red Bronze'], femaleGenotype: ['bb Rr Spsp'], femaleRatio: [50] } },
+
+
+{ sire: 'Nebraskan', dam: 'Black', results: { male: ['Barred Black'], maleGenotype: ['Bb Spsp'], maleRatio: [50], female: ['Barred Black'], femaleGenotype: ['Bb Spsp'], femaleRatio: [50] } },
+
+{ sire: 'Nebraskan', dam: 'Blue Red Bronze', results: { male: ['Red Slate', 'Blue Red Bronze', 'Bronze', 'Red Bronze'], maleGenotype: ['bb Dd Spsp', 'bb Dd Rr Spsp', 'bb Spsp', 'bb Rr Spsp'], maleRatio: [12.5, 12.5, 12.5, 12.5], female: ['Red Slate', 'Blue Red Bronze', 'Bronze', 'Red Bronze'], femaleGenotype: ['bb Dd Spsp', 'bb Dd Rr Spsp', 'bb Spsp', 'bb Rr Spsp'], femaleRatio: [12.5, 12.5, 12.5, 12.5] } },
+
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
