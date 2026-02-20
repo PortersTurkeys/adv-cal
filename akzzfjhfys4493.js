5f5e4eb39c0ca5594db04093a3013ae8be5381b7
PortersTurkeys <portersrareturkeys@gmail.com>
2026-02-20 14:26:34 -0500

    Update akzzfjhfys4493.js

--- akzzfjhfys4493.js
+++ akzzfjhfys4493.js
@@ -1,37 +1,7 @@
 const scripts = [
    
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-AlleleCombos-Ratios.js",
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-CalculateOffspring.js",
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-DisplayResults.js",  
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-Sounds.js",
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-SocialShare.js",
-"https://portersturkeys.github.io/adv-cal/VariousFunctions-LatestFunctions.js",  
 
-  
-  
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1B.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1C.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1D.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping1E.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping2.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping2A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping3.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping3A.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping4.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping5.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping6.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping7.js",
 "https://portersturkeys.github.io/adv-cal/geno-pheno-mapping7A.js",  
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping8.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping9.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping10.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping11.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping12.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping13.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping14.js",
-"https://portersturkeys.github.io/adv-cal/geno-pheno-mapping15.js"
 
 ];
 
